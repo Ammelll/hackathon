3a37eafa0f1215dd2cacebbdc774175b13413247
Ammel <77129284+Ammelll@users.noreply.github.com>
2026-03-17 18:41:16 +0000

    movement is now acceleration based

--- script.js
+++ script.js
@@ -3,24 +3,40 @@ function setup() {
     p = new Player(100,100)
   createCanvas(window.innerWidth, window.innerHeight);
 }
+class Velocity{
+    constructor(x,y){
+        this.vx = x;
+        this.vy = y;
+    }
+}
 class Player{
     constructor(x,y){
         this.x = x;
         this.y = y;
+        this.v = new Velocity(0,0);
+        this.acceleration = 0.5;
+        this.maxSpeed = 10;
     }
     update(){
         p.draw()
+        if(abs(p.v.vx) > p.maxSpeed){
+            p.v.vx = Math.min(Math.max(p.v.vx, -p.maxSpeed), p.maxSpeed);
+        }
+        console.log(this.v.vx)
+        p.x+=this.v.vx;
+
+        p.y+=this.v.vy
         if (keyIsDown(37)) {
-            p.x -= 5;
+            p.v.vx -= p.acceleration;
         }
         if (keyIsDown(39)) {
-            p.x += 5;
+            p.v.vx += p.acceleration;
         }
         if (keyIsDown(38)) {
-            p.y -= 5;
+            p.v.vy -= p.acceleration;
         }
         if (keyIsDown(40)) {
-            p.y += 5;
+            p.v.vy += p.acceleration;
         }
     }
     draw(){
