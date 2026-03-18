1edc6cd46351197d80e65d33cf5fda8ce4e556f9
Ammel <77129284+Ammelll@users.noreply.github.com>
2026-03-18 18:15:20 +0000

    ball collision

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-let p;
-function setup() {
-    p = new Player(100,100)
-  createCanvas(window.innerWidth, window.innerHeight);
+const print = console.log
+class Rectangle{
+    constructor(x,y,w,h){
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
 }
 class Velocity{
     constructor(x,y){
@@ -15,14 +19,19 @@ class Player{
         this.y = y;
         this.v = new Velocity(0,0);
         this.acceleration = 0.5;
-        this.maxSpeed = 10;
+        this.maxSpeed = 5;
+        this.radius = 25;
     }
     update(){
+        p.collisions()
         p.draw()
+        p.gravity()
         if(abs(p.v.vx) > p.maxSpeed){
             p.v.vx = Math.min(Math.max(p.v.vx, -p.maxSpeed), p.maxSpeed);
         }
-        console.log(this.v.vx)
+        if(abs(p.v.vy) > p.maxSpeed){
+            p.v.vy = Math.min(Math.max(p.v.vy, -p.maxSpeed), p.maxSpeed);
+        }
         p.x+=this.v.vx;
 
         p.y+=this.v.vy
@@ -40,14 +49,38 @@ class Player{
         }
     }
     draw(){
-        circle(p.x,p.y,50)
+        circle(p.x,p.y,p.radius*2)
     }
     keyPressed(){
          
     }
+    collisions(){
+        for(let r of rectangles){
+            print( )
+            if (p.x-p.radius < r.x+r.w && p.x+p.radius > r.x &&
+                p.y+p.radius > r.y && p.y-p.radius < r.y+r.h ){
+                    p.v.vy = -p.v.vy
+                }
+        }
+    }
+    gravity(){
+        p.v.vy+=0.05
+    }
+}
+
+let p;
+let rectangles = [new Rectangle(0,500,1000,100)];
+function setup() {
+    p = new Player(100,100)
+  createCanvas(window.innerWidth, window.innerHeight);
 }
 function draw() {
   background(220);
   p.update()
- 
+  drawRectangles();
+}
+function drawRectangles(){
+    for(let r of rectangles){
+        rect(r.x,r.y,r.w,r.h)
+    }
 }
