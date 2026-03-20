1d5a29a3b702ad4d8588edeae0cdf684c0885cbd
Ammel <77129284+Ammelll@users.noreply.github.com>
2026-03-20 00:35:41 +0000

    level 1

--- foo.js
+++ foo.js
@@ -3,7 +3,8 @@ const print = console.log
 let p;
 const width = window.innerWidth
 const height = window.innerHeight
-let rectangles = [new Rectangle(width*2/3,height/2,width/3)];
+let rectangles = [new Rectangle(width*11/12,height/2,width/6), new Rectangle(width*6/5,height/2,width*1/3),new Rectangle(width*13/10,height/2-100,60,100)];
+
 // setInterval(() => rectangles.filter(r => r.x + r.w >= 0), 2000);
 // setInterval(() => rectangles.push(new Rectangle(1000)), 10000);
 
@@ -11,7 +12,7 @@ function preload() {
   img = loadImage('/assets/rb.png');
 }
 function setup() {
-    p = new Player(window.innerWidth/2-300, window.innerHeight/2-300,img)
+    p = new Player(window.innerWidth/2, window.innerHeight/2-300,img)
     createCanvas(window.innerWidth, window.innerHeight);
 }
 function draw() {
--- player.js
+++ player.js
@@ -2,6 +2,7 @@ const LEFT = 37;
 const RIGHT = 39;
 const UP = 38;
 const DOWN = 40;
+const AIR_RESISTANCE_FACTOR = 0.08
 class Player {
     constructor(x, y,sprite) {
         this.x = x;
@@ -9,12 +10,13 @@ class Player {
         this.rx = x;
         this.ry = y;
         this.v = new Velocity(0, 0);
-        this.acceleration = 0.5;
+        this.acceleration = 1;
         this.maxSpeed = 5;
         this.radius = 25;
         this.isGround = false;
         this.sprite = sprite;
         this.angle = 0;
+        this.isWall = false;
     }
     update() {
         p.collisions()
@@ -25,12 +27,10 @@ class Player {
     }
     playerInputs() {
        if (keyIsDown(LEFT)) {
-            p.v.vx -= p.acceleration;
-            this.angle-=5
+            p.v.vx -= p.acceleration * this.isGround ? 1 : AIR_RESISTANCE_FACTOR;
         }
         if (keyIsDown(RIGHT)) {
-            p.v.vx += p.acceleration;
-            this.angle+=5
+            p.v.vx += p.acceleration * this.isGround ? 1 : AIR_RESISTANCE_FACTOR;
         }
         if (keyIsDown(UP) && this.isGround) {
             p.v.vy -= 5;
@@ -43,20 +43,25 @@ class Player {
         if (abs(p.v.vy) > p.maxSpeed) {
             p.v.vy = Math.min(Math.max(p.v.vy, -p.maxSpeed), p.maxSpeed);
         }
+        p.angle+=this.v.vx
+        if(this.isWall){
+            this.v.vx = 0
+        }
         p.x += this.v.vx;
 
         if(this.isGround && this.v.vy > 0){
             this.v.vy = 0
         }
+
         p.y +=this.v.vy
     }
     draw() {
         imageMode(CENTER);
-        translate(window.innerWidth/2-300, window.innerHeight/2-300);
+        translate(window.innerWidth/2, window.innerHeight/2-300);
         rotate(PI/180 * this.angle);
         image(img, 0, 0, p.radius*2, p.radius*2);
         rotate(-PI / 180 * this.angle);
-        translate(-(window.innerWidth/2-300), -(window.innerHeight/2-300));
+        translate(-(window.innerWidth/2), -(window.innerHeight/2-300));
 
         // image(img, p.x-p.radius, p.y-p.radius,p.radius*2,p.radius*2);
     }
@@ -66,10 +71,9 @@ class Player {
     collisions() {
         this.isGround = false
         for (let r of rectangles) {
-            print(p.rx,r.x,r.x+r.w,p.y+p.radius,r.y)
             if (p.rx - p.radius < r.x + r.w && p.rx + p.radius > r.x &&
-                p.ry + p.radius > r.y && p.ry - p.radius < r.y + r.h) {
-                this.isGround = true;
+                p.ry - p.radius < r.y + r.h && p.ry + p.radius > r.y) {
+                    this.isGround = true;
             }
         }
     }
